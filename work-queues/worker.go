d162603c48058f3daddff3457cbdf017aef78d8d
jhands0 <jamie_hands0@protonmail.com>
2025-03-11 10:38:58 +0000

    Created a consumer that receives command line arguments

---
+++ work-queues/worker.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+    "log"
+    "os"
+    "time"
+    
+    amqp "github.com/rabbitmq/amqp091-go"
+    "github.com/joho/godotenv"
+)
+
+func failOnError(err error, msg string) {
+    if err != nil {
+        log.Panicf("%s: %s", msg, err)
+    }
+}
+
+func main() {
+    // Loading RabbitMQ env variable
+    err := godotenv.Load("../.env")
+    failOnError(err, "Failed to load enviroment variables")
+    addr := os.Getenv("RABBITMQ_ADDR")
+    
+    // Creating a connection, channel and declaring a channel queue
+    conn, err := amqp.Dial(addr)
+    failOnError(err, "Failed to connect to RabbitMQ")
+    defer conn.Close()
+    
+    ch, err := conn.Channel()
+    failOnError(err, "Failed to open a channel")
+    defer ch.Close()
+    
+    q, err := ch.QueueDeclare("hello", false, false, false, false, nil)
+    failOnError(err, "Failed to declare a queue")
+    
+    
+    // Registered a consumer to receive messages
+    msgs, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+    failOnError(err, "Failed to register a consumer")
+    
+    // Creating a channel for all our threads to access
+    var forever chan struct{}
+    
+    // Everytime a message is received, a new thread is spun up
+    go func() {
+        for d := range msgs {
+            log.Printf("Received a message: %s", d.Body)
+            dotCount := bytes.Count(d.body, []bytes("."))
+            t := time.Duration(dotCount)
+            time.Sleep(t * time.Second)
+            log.Printf("Done")
+        }
+    }()
+    
+    log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+    <-forever
+    
+    
+}
