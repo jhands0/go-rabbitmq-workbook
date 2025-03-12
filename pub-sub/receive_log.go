240c4c2e3754fbe318240630781a3a983a580472
jhands0 <jamie_hands0@protonmail.com>
2025-03-12 15:27:08 +0000

    Created emit_logs.go, a consumer that uses both an exchange and a queue

---
+++ pub-sub/receive_log.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+    "log"
+    "os"
+    
+    amqp "github.com/rabbitmq/amqp091-go"
+    "github.com/joho/godotenv"
+)
+
+func failOnError(err error, msg string) {
+    if err != nil {
+        log.Panicf("%s: %s", msg, err)
+    }
+}
+
+func main() {
+    // Loading RabbitMQ env variable
+    err := godotenv.Load("../.env")
+    failOnError(err, "Failed to load enviroment variables")
+    addr := os.Getenv("RABBITMQ_ADDR")
+    
+    // Creating a connection, channel and declaring a channel queue
+    conn, err := amqp.Dial(addr)
+    failOnError(err, "Failed to connect to RabbitMQ")
+    defer conn.Close()
+    
+    ch, err := conn.Channel()
+    failOnError(err, "Failed to open a channel")
+    defer ch.Close()
+    
+    err = ch.ExchangeDeclare("logs", "fanout", true, false, false, false, nil)
+    failOnError(err, "Failed to declare an exchange")
+    
+    q, err := ch.QueueDeclare("", false, false, true, false, nil)
+    failOnError(err, "Failed to declare a queue")
+    
+    err = ch.QueueBind(q.Name, "", "logs", false, nil)
+    failOnError(err, "Failed to bind a queue")
+    
+    // Registered a consumer to receive messages
+    msgs, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+    failOnError(err, "Failed to register a consumer")
+    
+    // Creating a channel for all our threads to access
+    var forever chan struct{}
+    
+    // Creates a second thread to respond to all messages received
+    go func() {
+        for d := range msgs {
+            log.Printf(" [x] %s", d.Body)
+        }
+    }()
+    
+    log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+    <-forever
+}
